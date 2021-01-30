e38ea6c3c6de3ad78f38eac2a3ac2b06b75d564f
Omolola-Richard <omololarichard@gmail.com>
2021-01-30 13:35:27 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[]args){
         System.out.println("Hello, my name is Narindra");
         System.out.println("Hello Narindra, my name is Oluwakemi");
-        //Someone else can add another line here
+        System.out.println("Hello Narindra, my name is Omolola");
+        
     }
 }
