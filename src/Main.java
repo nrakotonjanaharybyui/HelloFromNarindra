ae749dcf22bc9f29d4c5000c25eda4133ab7b34c
Narindra Rakoto <rakoto@gmail.com>
2021-01-30 14:56:29 +0300

    Added some another line

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[]args){
         System.out.println("Hello, my name is Narindra");
         System.out.println("Hello Narindra, my name is Oluwakemi");
+        //Someone else can add another line here
     }
 }
